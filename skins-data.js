8fe09d19a152bcf0790d1b6b1c3f836b52257e79
Schwanz ML <riskisigma1322@gmail.com>
2025-07-03 18:04:12 +0700

    Update skins-data.js

--- skins-data.js
+++ skins-data.js
@@ -2,124 +2,148 @@ const skins = [
   {
     img: 'https://i.ibb.co/Fb5bZFPY/Chou-Libra-Shiryu.jpg',
     name: 'Chou Saint Seiya',
-    date: '26-6-2025',
+    date: '26-06-2025',
     link: 'chou-saint-seiya.html'
   },
   {
     img: 'https://i.ibb.co/35d4fF98/20250625-073352.jpg',
     name: 'Alucard Luckybox',
-    date: '26-6-2025',
+    date: '26-06-2025',
     link: 'skin-detail.html?hero=alucard-luckybox'
   },
   {
     img: 'https://i.ibb.co/VW5tDKKb/Natan-Archon-of-Knowledge.jpg',
     name: 'Natan MSC',
-    date: '21-6-2025',
+    date: '21-06-2025',
     link: 'skin-detail.html?hero=natan-msc'
   },
   {
     
     img: 'https://i.ibb.co/GrZdFFq/Luo-Yi-Starborne-Matron.jpg',
     name: 'Luo Yi Create',
-    date: '21-6-2025',
+    date: '21-06-2025',
     link: 'skin-detail.html?hero=luo-yi-create'
   },
   {
     img: 'https://i.ibb.co/m5QdjHSf/Lesley-Meowkin-Hunter.jpg',
     name: 'Lesley Fatcat',
-    date: '20-6-2025',
+    date: '20-06-2025',
     link: 'skin-detail.html?hero=lesley-fatcat'
   },
   {
     img: 'https://i.ibb.co/SD38J6M3/Julian-Meowkin-Tracker.jpg',
     name: 'Julian Fatcat',
-    date: '19-6-2025',
+    date: '19-06-2025',
     link: 'skin-detail.html?hero=julian-fatcat'
   },
   {
     img: 'https://i.ibb.co/4RJ6rB6N/20250702-132213.jpg',
     name: 'Arlott Collector',
-    date: '07-6-2025',
+    date: '07-06-2025',
     link: 'skin-detail.html?hero=arlott-collector'
   },
   {
     img: 'https://i.ibb.co/0jCdJ2bB/Jawhead-Proto-Guardian.jpg',
     name: 'Jawhead Allstar',
-    date: '30-5-2025',
+    date: '30-05-2025',
     link: 'skin-detail.html?hero=jawhead-allstar'
   },
   {
     img: 'https://i.ibb.co/PzvgTbft/Silvanna-Meowkin-Warden.jpg',
     name: 'Silvanna Fatcat',
-    date: '29-5-2025',
+    date: '29-05-2025',
     link: 'skin-detail.html?hero=silvanna-fatcat'
   },
   {
     img: 'https://i.ibb.co/YF0PcP1F/Lukas-Naruto-Uzumaki.jpg',
     name: 'Lukas X Naruto',
-    date: '04-5-2025',
+    date: '04-05-2025',
     link: 'skin-detail.html?hero=lukas-naruto'
   },
     {
     img: 'https://i.ibb.co/fz503WJf/Chou-Iori-Yagami.jpg',
     name: 'Chou Kof',
-    date: '29-6-2025',
-    link: '#'
+    date: '29-06-2025',
+    link: 'skin-detail.html?hero=chou-kof'
   },
   {
     img: 'https://i.ibb.co/zV9vNGV3/Chou-ECHO.jpg',
     name: 'Chou Echo',
-    date: '29-6-2025',
-    link: '#'
+    date: '29-06-2025',
+    link: 'skin-detail.html?hero=chou-echo'
   },
     {
     img: 'https://i.ibb.co/sJjgRmN0/Chou-S-T-U-N-Chou.jpg',
     name: 'Chou Stun',
-    date: '29-6-2025',
-    link: '#'
+    date: '29-06-2025',
+    link: 'skin-detail.html?hero=chou-stun'
   },
       {
     img: 'https://i.ibb.co/ycnVVZjt/Alucard-Obi-Wan-Kenobi.jpg',
     name: 'Alucard Starwars',
-    date: '29-6-2025',
-    link: '#'
+    date: '29-06-2025',
+    link: 'skin-detail.html?hero=alucard-starwars'
   },
   {
     img: 'https://i.ibb.co/mF9Y2ftd/Alucard-Obsidian-Blade.jpg',
     name: 'Alucard Legend',
-    date: '29-6-2025',
-    link: '#'
+    date: '29-06-2025',
+    link: 'skin-detail.html?hero=alucard-legend'
   },
     
     {
     img: 'https://i.ibb.co/W4RGgvPt/Ling-Neobeast-Ling.jpg',
     name: 'Ling Neobeast',
-    date: '02-5-2025',
+    date: '02-05-2025',
     link: 'skin-detail.html?hero=ling-neobeast'
   },
   {
     img: 'https://i.ibb.co/MkZyNTLC/Suyou-Sasuke-Uchiha.jpg',
     name: 'Suyou X Sasuke',
-    date: '28-4-2025',
+    date: '28-04-2025',
     link: 'skin-detail.html?hero=suyou-sasuke'
   },
     {
     img: 'https://i.ibb.co/4nX94sBQ/Kimmy-First-Order-Jet-Trooper.jpg',
     name: 'Kimmy Starwars',
-    date: '25-4-2025',
+    date: '25-04-2025',
     link: 'skin-detail.html?hero=kimmy-starwars'
   },
       {
     img: 'https://i.ibb.co/93C2GN3X/Yi-Sun-shin-Lone-Destructor.jpg',
     name: 'Yss Collector',
-    date: '24-4-2025',
-    link: '#'
+    date: '24-04-2025',
+    link: 'skin-detail.html?hero=yss-collector'
   },
   {
     img: 'https://i.ibb.co/vxKwVgFw/Vale-Gaara.jpg',
     name: 'Vale X Gaara',
-    date: '12-4-2025',
-    link: '#'
+    date: '12-04-2025',
+    link: 'skin-detail.html?hero=vale-gaara'
   },
-
+{
+    img: 'https://i.ibb.co/C5m20D8T/Zhask-Bone-Flamen.jpg',
+    name: 'Zhask Luckybox',
+    date: '12-04-2025',
+    link: 'skin-detail.html?hero=suyou-sasuke'
+  },
+    {
+    img: 'https://i.ibb.co/LDDpTJ8R/Hanzo-Soulfire-Netherlord.jpg',
+    name: 'Hanzo Collector',
+    date: '11-04-2025',
+    link: 'skin-detail.html?hero=kimmy-starwars'
+  },
+      {
+    img: 'https://i.ibb.co/VWW61LsZ/Hayabusa-Kakashi-Hatake.jpg',
+    name: 'Hayabusa X Kakashi',
+    date: '11-04-2025',
+    link: 'skin-detail.html?hero=hayabusa-kakashi'
+  },
+  {
+    img: 'https://i.ibb.co/dJG7G585/Kalea-Sakura-Haruno.jpg',
+    name: 'Kalea X Sakura',
+    date: '11-04-2025',
+    link: 'skin-detail.html?hero=kalea-sakura'
+  },
+  
 ];
